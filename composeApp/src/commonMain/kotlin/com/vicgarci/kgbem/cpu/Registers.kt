21b9ddd68381a8896d78235598eba1ba95db07eb
Víctor J García <vjgarciaw@gmail.com>
2025-09-23 13:03:46 +0100

    Initial flags register API

--- composeApp/src/commonMain/kotlin/com/vicgarci/kgbem/cpu/Registers.kt
+++ composeApp/src/commonMain/kotlin/com/vicgarci/kgbem/cpu/Registers.kt
@@ -27,3 +27,32 @@ data class Registers(
         return ((left.toInt().shl(8)) or (right.toInt())).and(0xFFFF).toUShort()
     }
 }
+
+data class FlagsRegister(
+    val zero: Boolean,
+    val subtract: Boolean,
+    val halfCarry: Boolean,
+    val carry: Boolean,
+) {
+
+    companion object {
+
+        private const val ZERO_FLAG_BYTE_POSITION = 7
+        private const val SUBTRACT_FLAG_BYTE_POSITION = 6
+        private const val HALF_CARRY_FLAG_BYTE_POSITION = 5
+        private const val CARRY_FLAG_BYTE_POSITION = 4
+
+        fun from(register: UByte): FlagsRegister {
+            return FlagsRegister(
+                zero = register.toInt().shr(ZERO_FLAG_BYTE_POSITION).and(0b1) != 0,
+                subtract = register.toInt().shr(SUBTRACT_FLAG_BYTE_POSITION).and(0b1) != 0,
+                halfCarry = register.toInt().shr(HALF_CARRY_FLAG_BYTE_POSITION).and(0b1) != 0,
+                carry = register.toInt().shr(CARRY_FLAG_BYTE_POSITION).and(0b1) != 0,
+            )
+        }
+
+        fun UByte.toFlagsRegister(): FlagsRegister {
+            return from(this)
+        }
+    }
+}
--- composeApp/src/commonTest/kotlin/com/vicgarci/kgbem/cpu/RegistersTest.kt
+++ composeApp/src/commonTest/kotlin/com/vicgarci/kgbem/cpu/RegistersTest.kt
@@ -1,5 +1,6 @@
 package com.vicgarci.kgbem.cpu
 
+import com.vicgarci.kgbem.cpu.FlagsRegister.Companion.toFlagsRegister
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -40,6 +41,59 @@ class RegistersTest {
                 b = 0x10.toUByte(),
                 c = 0x01.toUByte(),
             ).bc,
+        )
+    }
+
+    @Test
+    fun flagsRegisterFromRegister() {
+        assertEquals(
+            FlagsRegister(
+                zero = true,
+                subtract = false,
+                halfCarry = false,
+                carry = false,
+            ),
+            ALL_ZERO.copy(f = 0b10000000.toUByte()).f.toFlagsRegister(),
+        )
+
+        assertEquals(
+            FlagsRegister(
+                zero = false,
+                subtract = true,
+                halfCarry = false,
+                carry = false,
+            ),
+            ALL_ZERO.copy(f = 0b01000000.toUByte()).f.toFlagsRegister(),
+        )
+
+        assertEquals(
+            FlagsRegister(
+                zero = false,
+                subtract = false,
+                halfCarry = true,
+                carry = false,
+            ),
+            ALL_ZERO.copy(f = 0b00100000.toUByte()).f.toFlagsRegister(),
+        )
+
+        assertEquals(
+            FlagsRegister(
+                zero = false,
+                subtract = false,
+                halfCarry = false,
+                carry = true,
+            ),
+            ALL_ZERO.copy(f = 0b00010000.toUByte()).f.toFlagsRegister(),
+        )
+
+        assertEquals(
+            FlagsRegister(
+                zero = true,
+                subtract = true,
+                halfCarry = true,
+                carry = true,
+            ),
+            ALL_ZERO.copy(f = 0b11110000.toUByte()).f.toFlagsRegister(),
         )
     }
 }
